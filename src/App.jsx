bb0276c153a9f851a925910a6a0a12bcf881bd73
Prat <99562855+Prat-The-Brat@users.noreply.github.com>
2023-06-05 21:21:13 +0530

    tp

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+const App = () => {
   return (
-    <>
-    <div>
-      CabApp
-    </div>
-    </>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
   )
 }
 
