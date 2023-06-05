4398e92fb32168705c4847855d820dc796bdb79e
RanSomeWhere <samarth3tankasali@gmail.com>
2023-06-06 01:25:26 +0530

    final navbar changes and fixed landing page grid

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,28 +1,27 @@
-import './Landing.css';
+import "./Landing.css";
 import React from "react";
-import Car from '../assets/car.svg';
+import Car from "../assets/car.svg";
 
 const Landing = () => {
-    return (
-        <div className='container'>
-            <div className='maintext'>
-            One stop solution to manage your Cabs and Drivers.
-            </div>
-            <div className='w-9 h-9 objects-contain'>
-                <img src={Car} className='car'/>
-            </div>
-            <div className='subtext'>
-            At GEDI, we are dedicated to providing you with a comprehensive solution to streamline your cab and driver operations.
-            </div> 
-            <div>
-            </div>
-            <button
-            className="getstartedB"
-            >
-            G E T &nbsp; S T A R T E D !
-            </button>
+  return (
+    <div className="container">
+      <div className="maintext-container">
+        <div className="maintext">
+          One stop solution to manage your Cabs and Drivers.
         </div>
-    )
-}
+        <div className="subtext">
+          At GEDI, we are dedicated to providing you with a comprehensive
+          solution to streamline your cab and driver operations.
+        </div>
+        <div className="get-started-btn">
+          <button className="getstartedB">G E T &nbsp; S T A R T E D !</button>
+        </div>
+      </div>
+      <div className="w-9 h-9 objects-contain">
+        <img src={Car} className="car" />
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
     <Disclosure as="nav" className="gedi-navbar bg-navbar h-[6.5rem]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 bg-navbar">
+            <div className="relative flex h-16 items-center justify-between bg-navbar">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 {/* <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -40,12 +40,12 @@ const Navbar = () => {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="block h-90 lg:hidden logo-position"
+                    className="block h-90 lg:hidden logo-position bg-navbar"
                     src={logo}
                     alt="Your Company"
                   />
                   <img
-                    className="hidden h-90 lg:block logo-position"
+                    className="hidden h-90 lg:block logo-position bg-navbar"
                     src={logo}
                     alt="Your Company"
                   />
@@ -69,13 +69,13 @@ const Navbar = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
+                {/* <button
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="sr-only">View notifications</span>
-                  {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
-                </button>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button> */}
 
                 {/* Profile dropdown */}
                 {/* <Menu as="div" className="relative ml-3">
@@ -160,4 +160,4 @@ const Navbar = () => {
   )
 }
 
-export { Navbar }
+export default Navbar 
