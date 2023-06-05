58bc35436158edfc637d13d3a590b47be30585b6
n1ti <niti200005@gmail.com>
2023-06-05 22:54:36 +0530

    Lund

---
+++ src/components/Landing.jsx
@@ -0,0 +1,25 @@
+import './Landing.css';
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div>
+            <div className='maintext'>
+            One stop solution to manage 
+            your Cabs and Drivers.
+            </div> 
+            <div className='subtext'>
+            At GEDI, we are dedicated to providing you with a comprehensive solution to streamline your cab and driver operations.
+            </div> 
+            <div>
+            </div>
+            <button
+            className="getstartedB"
+            >
+            GET STARTED!
+            </button>
+        </div>
+    )
+}
+
+export default Landing;
