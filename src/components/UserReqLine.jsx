912aa25de488be8c4fa2c6568c43041f5ef03b6e
Pratham Bhonge <prathambhonge@gmail.com>
2023-07-15 14:18:59 +0530

    sexy pratham

--- src/components/UserReqLine.jsx
+++ src/components/UserReqLine.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import DeleteIcon from "../assets/delete.svg";
-import EditProfileIcon from "../assets/editprofile.svg";
 import "./UserRequests.css";
 
-function UserReqLine(props) {
+function UserReqLine({ tripId, cname, driverId, cabId, onStartTrip }) {
+  const handleStartTripClick = () => {
+    onStartTrip(tripId);
+  };
+
   return (
     <tr className="datarowoutlines">
-      <td className="tablerows tripid">{props.tripid}</td>
-      <td className="tablerows cname">{props.cname}</td>
-      <td className="tablerows driverid">{props.driverid}</td>
-      <td className="tablerows cabid">{props.cabid}</td>
+      <td className="tablerows tripid">{tripId}</td>
+      <td className="tablerows cname">{cname}</td>
+      <td className="tablerows driverid">{driverId}</td>
+      <td className="tablerows cabid">{cabId}</td>
       <td className="tablerows op">
         <div>
-          <button className="activeBtn">Start Trip</button>
+          <button className="activeBtn" onClick={handleStartTripClick}>
+            Start Trip
+          </button>
         </div>
       </td>
     </tr>
